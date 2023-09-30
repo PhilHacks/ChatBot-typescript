b8d6a1a0249ac234e2921d34cc05fe60619bc4f0
Phil <filip.nyman7@gmail.com>
2023-09-30 19:09:40 +0200

    "removed comments" no comment will show up in js

--- ChatBot.js
+++ ChatBot.js
@@ -1,26 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ChatBot = void 0;
-//Command classes (implements the interface rules)
 class HelpCommand {
-  execute(input) {
-    return "Help text";
-  }
-}
-//ChatBot Class (uses command classes to execute commands)
-class ChatBot {
-  executeCommand(command) {
-    switch (command) {
-      case "help":
-        return "Here is the help text.";
-      case "greet":
-        return "Greet text.";
-      case "weather":
-        //logik för att hämta väder
-        return "The weather is sunny with a high of 25°C.";
-      default:
-        return "Unknown command.";
+    execute(input) {
+        return "Help text";
     }
-  }
+}
+class ChatBot {
+    executeCommand(command) {
+        switch (command) {
+            case "help":
+                return "Here is the help text.";
+            case "greet":
+                return "Greet text.";
+            case "weather":
+                return "The weather is sunny with a high of 25°C.";
+            default:
+                return "Unknown command.";
+        }
+    }
 }
 exports.ChatBot = ChatBot;
