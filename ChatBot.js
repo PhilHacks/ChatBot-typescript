e95abd1b8361f16c1af3ceac606ab365d902d770
Phil <filip.nyman7@gmail.com>
2023-09-30 18:42:07 +0200

    added inteface in ChatBot file

--- ChatBot.js
+++ ChatBot.js
@@ -1,17 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ChatBot = void 0;
-//ChatBot Class.
+//ChatBot Class. 
 class ChatBot {
-  constructor() {}
-  //Methods for commands:
-  executeCommand(command) {
-    switch (command) {
-      case "help":
-        return "Here is the help text.";
-      default:
-        return "Unknown command.";
+    constructor() {
     }
-  }
+    //Methods for commands: 
+    executeCommand(command) {
+        switch (command) {
+            case "help":
+                return "Here is the help text.";
+            case "greet":
+                return "Greet text.";
+            case "weather":
+                //logik för att hämta väder
+                return "The weather is sunny with a high of 25°C.";
+            default:
+                return "Unknown command.";
+        }
+    }
 }
 exports.ChatBot = ChatBot;
