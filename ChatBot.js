c163cda52e9459909632cb712cc77f0ae74a7b29
Phil <filip.nyman7@gmail.com>
2023-09-30 19:12:15 +0200

    refactored js file

--- ChatBot.js
+++ ChatBot.js
@@ -2,22 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ChatBot = void 0;
 class HelpCommand {
-    execute(input) {
-        return "Help text";
-    }
+  execute(input) {
+    return "Help text";
+  }
 }
 class ChatBot {
-    executeCommand(command) {
-        switch (command) {
-            case "help":
-                return "Here is the help text.";
-            case "greet":
-                return "Greet text.";
-            case "weather":
-                return "The weather is sunny with a high of 25°C.";
-            default:
-                return "Unknown command.";
-        }
+  executeCommand(command) {
+    switch (command) {
+      case "help":
+        return "Here is the help text.";
+      case "greet":
+        return "Greet text.";
+      case "weather":
+        return "The weather is sunny with a high of 25°C.";
+      default:
+        return "Unknown command.";
     }
+  }
 }
 exports.ChatBot = ChatBot;
