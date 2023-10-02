bd35271e7617d5408fb009ed76ac6c1a45dae3ca
Phil <filip.nyman7@gmail.com>
2023-10-02 13:43:25 +0200

    created private Command in CB class

--- ChatBot.js
+++ ChatBot.js
@@ -2,22 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ChatBot = void 0;
 class HelpCommand {
-  execute(input) {
-    return "Help text";
-  }
+    execute(input) {
+        return "Here is the help text";
+    }
+}
+class GreetCommand {
+    execute(input) {
+        return "Here is the greet text";
+    }
+}
+class WeatherCommand {
+    execute(input) {
+        return "Here is The weather sunny and 25°C.";
+    }
 }
 class ChatBot {
-  executeCommand(command) {
-    switch (command) {
-      case "help":
-        return "Here is the help text.";
-      case "greet":
-        return "Greet text.";
-      case "weather":
-        return "The weather is sunny with a high of 25°C.";
-      default:
-        return "Unknown command.";
+    constructor() {
+        this.helpCommand = new HelpCommand();
+        this.greetCommand = new GreetCommand();
+        this.weatherCommand = new WeatherCommand();
     }
-  }
+    executeCommand(command) {
+        switch (command) {
+            case "help":
+                return this.helpCommand.execute(command);
+            case "greet":
+                return this.greetCommand.execute(command);
+            case "weather":
+                return this.weatherCommand.execute(command);
+            default:
+                return "Unknown command.";
+        }
+    }
 }
 exports.ChatBot = ChatBot;
