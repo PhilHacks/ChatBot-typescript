57503b03e64235d1a63a9da7ee9f3332bf05cf2d
Phil <filip.nyman7@gmail.com>
2023-10-10 22:33:04 +0200

    put individual cmd classes in a group

--- tests/botMain.test.ts
+++ tests/botMain.test.ts
@@ -1,34 +1,32 @@
 import { HelpCommand, GreetCommand, WeatherCommand, ChatBot } from '../src/ChatBot';
 
 
-describe('Help command', () => {
-    it('should return a string with a help text', () => {
-        const help = new HelpCommand();
-        const result = help.execute("Help");
-        expect(result).toContain("Here is the help text")
+describe('Individual Command Classes', () => {
+    
+    describe('Help command', () => {
+        it('should return a string with a help text', () => {
+            const help = new HelpCommand();
+            const result = help.execute("Help");
+            expect(result).toContain("Here is the help text")
+        });
+    });
+
+    describe('Greet command', () => {
+        it('should return a string with a greet text', () => {
+            const greet = new GreetCommand();
+            const result = greet.execute("Greet");
+            expect(result).toContain("Here is the greet text")
+        });
+    });
+
+    describe('Weather command', () => {
+        it('should return a string with weather information', () => {
+            const weather = new WeatherCommand();
+            const result = weather.execute("Weather");
+            expect(result).toContain("Here is The weather: Sunny and 25°C.");
+        });
     });
 });
-
-
-describe('Greet command', () => {
-    it('should return a string with a greet text', () => {
-        const greet = new GreetCommand();
-        const result = greet.execute("Greet");
-        expect(result).toContain("Here is the greet text")
-    });
-});
-
-
-
-describe('Weather command', () => {
-    it('should return a string with weather information', () => {
-        const weather = new WeatherCommand();
-        const result = weather.execute("Weather");
-        expect(result).toContain("Here is The weather: Sunny and 25°C.");
-    });
-});
-
-
 
 describe('Unknown commands', () => {
     it('should return a string "unknown command"', () => {
