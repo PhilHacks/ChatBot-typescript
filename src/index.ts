4a54bb2af10854655b685bb942a283bf410d4ee2
Phil <filip.nyman7@gmail.com>
2023-10-11 21:43:56 +0200

    typed varible to Chatbot and change to es6 import

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const ChatBot = require('./ChatBot').ChatBot;
-const readline = require('readline/promises');
-const { stdin: input, stdout: output } = require('process');
+import { ChatBot } from "./ChatBot";
+import readline from 'readline/promises';
+import { stdin as input, stdout as output }  from 'process';
 
-const chatBotInstance = new ChatBot();
+const chatBotInstance: ChatBot = new ChatBot();
 
 
 async function main () {
